6c372ba2a2db95eacf14ca73bd43edfc794c296f
Cyrano webdev <cyrano@cyranowebdev.com>
2021-03-08 18:33:07 -0300

    ajustando app para dev label

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import StrangerThings from './components/StrangerThings';
 function App() {
   return (
     <div className="App">
-      { (process.env.DEV_MODE === 'true')
+      { (process.env.REACT_APP_DEV_MODE === 'true')
         ? <div className="dev-label">Em desenvolvimento</div>
         : null }
       <StrangerThings />
