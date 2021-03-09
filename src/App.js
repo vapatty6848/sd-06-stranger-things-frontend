02fd6954ae3a68305c1ed5524a9bb0d37bf8bba6
William Gomes <williamjog@hotmail.com>
2021-03-09 17:55:22 -0300

    9th requirement - development mode

--- src/App.js
+++ src/App.js
@@ -3,9 +3,13 @@ import './App.css';
 
 import StrangerThings from './components/StrangerThings';
 
+require('dotenv').config();
+
 function App() {
   return (
     <div className="App">
+      {process.env.REACT_APP_ENV && <h1>Em desenvolvimento</h1>}
+      <h1>{process.env.REACT_APP_ENV}</h1>
       <StrangerThings />
     </div>
   );
