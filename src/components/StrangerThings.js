4d7eecba9be2fae56177d7f332865f7c7d35ea2c
Mariano Ariel Diaz Elsztain <made@poli.ufrj.br>
2021-03-08 22:29:51 -0300

    env variables

--- src/components/StrangerThings.js
+++ src/components/StrangerThings.js
@@ -6,13 +6,13 @@ const getRealityClass = (hereIsTheUpsideDownWorld) => (
 );
 
 const strangerThingsConfig = {
-  url: 'http://localhost:3002',
-  timeout: 30000,
+  url: process.env.REACT_APP_HAWKINS_URL,
+  timeout: parseInt(process.env.REACT_APP_HAWKINS_TIMEOUT, 10),
 };
 
 const upsideDownConfig = {
-  url: 'http://localhost:3003',
-  timeout: 30000,
+  url: process.env.REACT_APP_UPSIDEDOWN_URL,
+  timeout: parseInt(process.env.REACT_APP_UPSIDEDOWN_TIMEOUT, 10),
 };
 
 const charactersService = new CharactersService(strangerThingsConfig);
