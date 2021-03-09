e6e0637b483a80ada6248c9e6743119ca91e97ba
LeonardoCavachini <cavachini240@gmail.com>
2021-03-09 18:37:42 -0300

    add varivel react-modo

--- src/App.js
+++ src/App.js
@@ -4,8 +4,11 @@ import './App.css';
 import StrangerThings from './components/StrangerThings';
 
 function App() {
+  const checkModo = (process.env.REACT_APP_MODE === 'development')
+    ? 'Em desenvolvimento' : null;
   return (
     <div className="App">
+      <p>{checkModo}</p>
       <StrangerThings />
     </div>
   );
