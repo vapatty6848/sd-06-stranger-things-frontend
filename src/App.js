c640895920e2435d9ed8e7d8c0c0ef2cf9458396
LeonardoCavachini <cavachini240@gmail.com>
2021-03-09 18:53:15 -0300

    modo production

--- src/App.js
+++ src/App.js
@@ -4,11 +4,8 @@ import './App.css';
 import StrangerThings from './components/StrangerThings';
 
 function App() {
-  const checkModo = (process.env.REACT_APP_MODE === 'development')
-    ? 'Em desenvolvimento' : null;
   return (
     <div className="App">
-      <p>{checkModo}</p>
       <StrangerThings />
     </div>
   );
