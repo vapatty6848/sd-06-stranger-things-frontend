92ebc96e2def06e203df6b6f5f36de2e9f4be52b
Clayton Miguel Da Silva <claytonmiguel1805@gmail.com>
2021-03-09 20:09:42 -0300

    mudando url no componente strangerThings para variaveis de ambiente e adicionando o arquivo .env

--- src/components/StrangerThings.js
+++ src/components/StrangerThings.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import CharactersService from '../services/charactersAPI';
 
+require('dotenv').config();
+
 const getRealityClass = (hereIsTheUpsideDownWorld) => (
   hereIsTheUpsideDownWorld ? 'upside-down' : 'stranger-things'
 );
 
 const strangerThingsConfig = {
-  url: 'http://localhost:3002',
-  timeout: 30000,
+  url: process.env.REACT_APP_HAWKINS_URL || 'http://localhost:3002',
+  timeout: process.env.REACT_APP_HAWKINS_TIMEOUT || 30000,
 };
 
 const upsideDownConfig = {
-  url: 'http://localhost:3003',
-  timeout: 30000,
+  url: process.env.REACT_APP_UPSIDEDOWN_URL || 'http://localhost:3003',
+  timeout: process.env.REACT_APP_UPSIDEDOWN_TIMEOUT || 30000,
 };
 
 const charactersService = new CharactersService(strangerThingsConfig);
