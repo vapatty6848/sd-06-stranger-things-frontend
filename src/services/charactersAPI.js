7be6b9ba8972930aeaf683c0228a3b625579b5a8
Alexandre Fogaça <alexandr.fogaca@gmail.com>
2021-03-09 16:11:11 -0300

    test

--- src/services/charactersAPI.js
+++ src/services/charactersAPI.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
+const URL = process.env.REACT_APP_HAWKINS_URL;
 const DEFAULT_TIMEOUT = 30000;
+
 class CharactersService {
   constructor({ url = 'http://localhost:3000', timeout = DEFAULT_TIMEOUT }) {
     this.http = axios.create({
