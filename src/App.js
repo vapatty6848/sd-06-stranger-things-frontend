8dfd4764fe2a6a1ce7b3d279e7644710b1656c09
rafMewtwo <ponciano.raf@gmail.com>
2021-03-09 20:55:04 -0300

    testando packt

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import React from 'react';
 import './App.css';
 
 import StrangerThings from './components/StrangerThings';
+require('dotenv/config');
 
 function App() {
+
   return (
     <div className="App">
+      {process.env.REACT_APP_DEV==='true' && <h1>Em desenvolvimento</h1>}
       <StrangerThings />
     </div>
   );
