319f8c0fab72cff8f19e2a2876c2d94f53f8b00e
vapatty6848 <vapatty@gmail.com>
2021-03-10 18:43:29 -0300

    novo app -ft

--- src/components/StrangerThings.js
+++ src/components/StrangerThings.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import CharactersService from '../services/charactersAPI';
+require('dotenv').config();
 
 const getRealityClass = (hereIsTheUpsideDownWorld) => (
   hereIsTheUpsideDownWorld ? 'upside-down' : 'stranger-things'
 );
 
 const strangerThingsConfig = {
-  url: REACT_APP_HAWKINS_URL,
-  timeout: REACT_APP_HAWKINS_TIMEOUT 
+  url: process.env.REACT_APP_HAWKINS_URL,
+  timeout:process.env. REACT_APP_HAWKINS_TIMEOUT 
 };
 
 const upsideDownConfig = {
-  url: REACT_APP_UPSIDEDOWN_URL,
-  timeout: REACT_APP_UPSIDEDOWN_TIMEOUT
+  url: process.env.REACT_APP_UPSIDEDOWN_URL,
+  timeout: process.env.REACT_APP_UPSIDEDOWN_TIMEOUT
 };
 
 const charactersService = new CharactersService(strangerThingsConfig);
