f3781ce113af8335493e2bf1ab42dff0884210c5
Jean Machado <jeanvictor.arq@gmail.com>
2021-03-09 21:59:16 -0300

    deploy

--- src/components/StrangerThings.js
+++ src/components/StrangerThings.js
@@ -7,14 +7,14 @@ const getRealityClass = (hereIsTheUpsideDownWorld) => (
 
 require('dotenv').config();
 
-const strangerThingsConfig = {
-  url: process.env.REACT_APP_HAWKINS_URL,
-  timeout: process.env.REACT_APP_HAWKINS_TIMEOUT,
-};
-
 const upsideDownConfig = {
   url: process.env.REACT_APP_UPSIDEDOWN_URL,
   timeout: process.env.REACT_APP_UPSIDEDOWN_TIMEOUT,
+};
+
+const strangerThingsConfig = {
+  url: process.env.REACT_APP_HAWKINS_URL,
+  timeout: process.env.REACT_APP_HAWKINS_TIMEOUT,
 };
 
 const charactersService = new CharactersService(strangerThingsConfig);
