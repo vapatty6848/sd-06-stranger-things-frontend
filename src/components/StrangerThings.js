8aa28e892b0973135991fb38b398dfb7d66c96ac
Alvaro Neto <alvaroshn@gmail.com>
2021-03-10 07:11:05 -0300

    changes bd test

--- src/components/StrangerThings.js
+++ src/components/StrangerThings.js
@@ -2,7 +2,8 @@ import React from 'react';
 import CharactersService from '../services/charactersAPI';
 
 require('dotenv').config();
-const { REACT_APP_HAWKINS_URL, REACT_APP_UPSIDEDOWN_URL } = process.env;
+const { REACT_APP_HAWKINS_URL, REACT_APP_HAWKINS_TIMEOUT, 
+  REACT_APP_UPSIDEDOWN_URL, REACT_APP_UPSIDEDOWN_TIMEOUT } = process.env;
 
 const getRealityClass = (hereIsTheUpsideDownWorld) => (
   hereIsTheUpsideDownWorld ? 'upside-down' : 'stranger-things'
@@ -10,12 +11,14 @@ const getRealityClass = (hereIsTheUpsideDownWorld) => (
 
 const strangerThingsConfig = {
   url: REACT_APP_HAWKINS_URL,
-  timeout: 30000,
+  // url: 'http://localhost:3002',
+  timeout: REACT_APP_HAWKINS_TIMEOUT,
 };
 
 const upsideDownConfig = {
   url:REACT_APP_UPSIDEDOWN_URL,
-  timeout: 30000,
+  // url: 'http://localhost:3003',
+  timeout: REACT_APP_UPSIDEDOWN_TIMEOUT,
 };
 
 const charactersService = new CharactersService(strangerThingsConfig);
@@ -67,10 +70,16 @@ class StrangerThings extends React.Component {
 
   searchCharacter(pages) {
     const { characterName, hereIsTheUpsideDownWorld, page } = this.state;
+   
     const service = hereIsTheUpsideDownWorld
       ? charactersUpsideDownService
       : charactersService;
 
+      console.log('É ', hereIsTheUpsideDownWorld)
+      console.log('caso true: charactersUpsideDownService')
+      console.log('caso falso: charactersService')
+      console.log('character name:',characterName)
+      console.log('paginas:',pages)
     const numberOfPages = 10;
     service
       .getCharacters(characterName, pages || page, numberOfPages)
