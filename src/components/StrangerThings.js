4def140c3a69fcbbef1863482eb742826ef0659d
deko possas <andrepossaster@gmail.com>
2021-03-10 18:26:52 -0300

    passa desgrama

--- src/components/StrangerThings.js
+++ src/components/StrangerThings.js
@@ -10,6 +10,8 @@ const strangerThingsConfig = {
   timeout: process.env.REACT_APP_HAWKINS_TIMEOUT,
 };
 
+// outro push
+
 const upsideDownConfig = {
   url: process.env.REACT_APP_UPSIDEDOWN_URL,
   timeout: process.env.REACT_APP_UPSIDEDOWN_TIMEOUT,
