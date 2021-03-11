8c5b54db7570f55d069261d1f88eadb337d3c0cd
Paulo Frazão <paulo4187@gmail.com>
2021-03-11 12:14:23 -0300

    Starting Project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import './App.css';
-
 import StrangerThings from './components/StrangerThings';
 
 function App() {
   return (
     <div className="App">
-      <StrangerThings />
+      <StrangerThings />    
     </div>
   );
 }
