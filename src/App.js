71274412a48b6ee6fc2e6eca7866419ab08d35a6
Gui-Alucard <guilhermeanv7822@gmail.com>
2021-03-11 10:50:03 -0300

    :sparkles: Desafios Front
    requisitos 7, 8 e 9

--- src/App.js
+++ src/App.js
@@ -3,9 +3,13 @@ import './App.css';
 
 import StrangerThings from './components/StrangerThings';
 
+require('dotenv').config();
+
 function App() {
   return (
     <div className="App">
+      {process.env.REACT_APP_ENV === 'development' && <h1>Em desenvolvimento</h1>}
+      {/* ajuda do william no plantão para o requisito 9 */}
       <StrangerThings />
     </div>
   );
