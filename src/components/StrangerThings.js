ec1da5a53f35f6d83fd0576a6adbacbdb9e7f66b
STEFANO-BRANZ <stefano.branz@gmail.com>
2021-03-13 18:34:59 -0300

    Frontend deploy for heroku 04

--- src/components/StrangerThings.js
+++ src/components/StrangerThings.js
@@ -6,12 +6,12 @@ const getRealityClass = (hereIsTheUpsideDownWorld) => (
 );
 
 const strangerThingsConfig = {
-  url: process.env.REACT_APP_HAWKINS_URL,
+  url: process.env.REACT_APP_HAWKINS_URL || 'http://localhost:3002',
   timeout: process.env.REACT_APP_HAWKINS_TIMEOUT,
 };
 
 const upsideDownConfig = {
-  url: process.env.REACT_APP_UPSIDEDOWN_URL,
+  url: process.env.REACT_APP_UPSIDEDOWN_URL || 'http://localhost:3003',
   timeout: process.env.REACT_APP_UPSIDEDOWN_TIMEOUT,
 };
 
