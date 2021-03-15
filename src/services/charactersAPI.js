e550790a3e538b7235d302c7c2f70899062972e6
Bruno Lemos <brunolemos.ssa@gmail.com>
2021-03-14 22:20:33 -0300

    change default url

--- src/services/charactersAPI.js
+++ src/services/charactersAPI.js
@@ -1,10 +1,9 @@
 import axios from 'axios';
 
 const DEFAULT_TIMEOUT = 30000;
-const DEFAULT_URL = process.env.REACT_APP_HAWKINS_URL;
 
 class CharactersService {
-  constructor({ url = DEFAULT_URL, timeout = DEFAULT_TIMEOUT }) {
+  constructor({ url = 'http://localhost:3000', timeout = DEFAULT_TIMEOUT }) {
     this.http = axios.create({
       baseURL: url,
       timeout,
