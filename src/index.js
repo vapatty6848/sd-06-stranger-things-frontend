cd10a5bc0776f9c2df2ff42e8397be2caff1a060
Kesley Lahoz <namielahoz@gmail.com>
2021-03-16 12:42:21 -0300

    trying again

--- src/index.js
+++ src/index.js
@@ -13,4 +13,5 @@ ReactDOM.render(
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
+// aoisdjaiosjd
 serviceWorker.unregister();
