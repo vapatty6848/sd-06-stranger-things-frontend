350a9ceaa5ae1a0bd1a5714ad9a8965663640e3b
Daniel Moura <darthur.moura@gmail.com>
2021-03-15 21:27:00 -0300

    Changes to api url

--- src/components/StrangerThings.js
+++ src/components/StrangerThings.js
@@ -10,13 +10,15 @@ const getRealityClass = (hereIsTheUpsideDownWorld) => (
 
 const strangerThingsConfig = {
   url:
-    process.env.REACT_APP_HAWKINS_URL,
+    process.env.REACT_APP_HAWKINS_URL
+    || 'https://darthurmoura-bk.herokuapp.com/',
   timeout: process.env.REACT_APP_HAWKINS_TIMEOUT,
 };
 
 const upsideDownConfig = {
   url:
-    process.env.REACT_APP_UPSIDEDOWN_URL,
+    process.env.REACT_APP_UPSIDEDOWN_URL
+    || 'https://darthurmoura-bd.herokuapp.com/',
   timeout: process.env.REACT_APP_UPSIDEDOWN_TIMEOUT,
 };
 
