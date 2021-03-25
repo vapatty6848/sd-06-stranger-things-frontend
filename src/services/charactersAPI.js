f5ed0a4dfa4bd396a0e2ea26971b1ba03bbcbe9c
afornagieri <fornagieri.artur@gmail.com>
2021-03-25 07:39:20 -0300

    fix branch name and app name

--- src/services/charactersAPI.js
+++ src/services/charactersAPI.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 const DEFAULT_TIMEOUT = 30000;
+
 class CharactersService {
   constructor({ url = 'http://localhost:3000', timeout = DEFAULT_TIMEOUT }) {
     this.http = axios.create({
