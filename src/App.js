e8491f2281a37bc3f62e5dedd82a438d71ab7874
afornagieri <fornagieri.artur@gmail.com>
2021-03-27 09:53:53 -0300

    fix frontend production

--- src/App.js
+++ src/App.js
@@ -3,15 +3,9 @@ import './App.css';
 
 import StrangerThings from './components/StrangerThings';
 
-const { REACT_APP_ENV_MODE } = process.env;
-
 function App() {
   return (
     <div className="App">
-      <p>
-        { REACT_APP_ENV_MODE === 'prod' ? 'Em produção' : null }
-        { REACT_APP_ENV_MODE === 'dev' ? 'Em desenvolvimento' : null }
-      </p>
       <StrangerThings />
     </div>
   );
