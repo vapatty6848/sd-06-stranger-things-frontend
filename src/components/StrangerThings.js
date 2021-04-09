74a5f72b03f7f246c9ea795f20aafbaef36b4e67
System Administrator <root@MacBook-Pro.local>
2021-04-09 13:33:33 -0300

    env/dotenv

--- src/components/StrangerThings.js
+++ src/components/StrangerThings.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import CharactersService from '../services/charactersAPI';
 
+require('dotenv').config();
+
 const getRealityClass = (hereIsTheUpsideDownWorld) => (
   hereIsTheUpsideDownWorld ? 'upside-down' : 'stranger-things'
 );
