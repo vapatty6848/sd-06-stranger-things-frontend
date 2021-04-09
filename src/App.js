aeaf7956c1c783b860ac9e92c25e99d0833c16c9
System Administrator <root@MacBook-Pro.local>
2021-04-09 14:00:41 -0300

    modo desenvolvimento

--- src/App.js
+++ src/App.js
@@ -3,9 +3,12 @@ import './App.css';
 
 import StrangerThings from './components/StrangerThings';
 
+require('dotenv').config();
+
 function App() {
   return (
     <div className="App">
+      {process.env.REACT_APP_ENV === 'development' && <h2>Em desenvolvimento</h2>}
       <StrangerThings />
     </div>
   );
