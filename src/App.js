eb9d6963e4fef1bd26d261dd2baad8b42abe77a0
Pedro Duarte <ikbenpedro@gmail.com>
2021-04-10 09:31:13 -0300

    adding variables and deploying

--- src/App.js
+++ src/App.js
@@ -6,11 +6,11 @@ import StrangerThings from './components/StrangerThings';
 require('dotenv').config();
 
 const developementMode = process.env.REACT_APP_ENV === 'development';
-const DevMode = developementMode && <h2> Em desenvolvimento</h2>;
+const RenderMode = developementMode && <h2> Em desenvolvimento</h2>;
 function App() {
   return (
     <div className="App">
-      {DevMode}
+      {RenderMode}
       <StrangerThings />
     </div>
   );
