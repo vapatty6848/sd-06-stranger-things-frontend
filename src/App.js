27d181e94fbba5b30d3bee11172d034412ceee4a
Pedro Duarte <ikbenpedro@gmail.com>
2021-04-10 08:57:46 -0300

    creating development mode

--- src/App.js
+++ src/App.js
@@ -3,11 +3,15 @@ import './App.css';
 
 import StrangerThings from './components/StrangerThings';
 
+require('dotenv').config();
+
+const developementMode = process.env.REACT_APP_ENV === 'development';
+const RenderMode = developementMode && <h2> Em desenvolvimento</h2>;
 function App() {
   return (
     <div className="App">
+      {RenderMode}
       <StrangerThings />
-      hello
     </div>
   );
 }
