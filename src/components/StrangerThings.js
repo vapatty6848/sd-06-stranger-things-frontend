eeb886bb7d2dfe207c8d2da26011f4dd00fb7f4f
Pedro Eduardo <pedroedu77@gmail.com>
2021-04-13 20:50:54 -0300

    Send to Heroku Front

--- src/components/StrangerThings.js
+++ src/components/StrangerThings.js
@@ -102,7 +102,7 @@ class StrangerThings extends React.Component {
       () => this.searchCharacter(),
     );
   }
-
+  // Development
   render() {
     const {
       hereIsTheUpsideDownWorld, characterName, characters, page,
