5a3d4e3b23b84d982db249060e33c10fa64b99d9
PAULO LINS <paulolins100@gmail.com>
2021-04-21 15:47:46 -0300

    refazendo-4

--- src/components/StrangerThings.js
+++ src/components/StrangerThings.js
@@ -106,10 +106,11 @@ class StrangerThings extends React.Component {
           hereIsTheUpsideDownWorld,
         )}` }
       >
-         <h2>{ process.env.REACT_APP_DEVELOPMENT_MODE === 'true' ?
-         'Em desenvolvimento' :
-         '' }
-         </h2>
+        <h2>
+          { process.env.REACT_APP_DEVELOPMENT_MODE === 'true' ?
+          'Em desenvolvimento' :
+          '' }
+        </h2>
         <div className="content strangerfy">
           <div className="change-reality">
             <button type="button" onClick={ this.changeRealityClick }>
