5208edc29bb69252ce6acbc0a3336cf4cab95b9c
PAULO LINS <paulolins100@gmail.com>
2021-04-21 16:09:39 -0300

    refazendo-5

--- src/components/StrangerThings.js
+++ src/components/StrangerThings.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import CharactersService from '../services/charactersAPI';
 
-require('dotenv').config();
-
 const getRealityClass = (hereIsTheUpsideDownWorld) => (
   hereIsTheUpsideDownWorld ? 'upside-down' : 'stranger-things'
 );
@@ -17,21 +15,28 @@ const upsideDownConfig = {
   timeout: process.env.REACT_APP_UPSIDEDOWN_TIMEOUT,
 };
 
+const developmentEnvironment = process.env.REACT_APP_DEVELOPMENT === 'true';
+
 const charactersService = new CharactersService(strangerThingsConfig);
 const charactersUpsideDownService = new CharactersService(upsideDownConfig);
+
 class StrangerThings extends React.Component {
   constructor(props) {
     super(props);
+
     this.state = {
       hereIsTheUpsideDownWorld: false,
       characterName: '',
       characters: [],
       page: 1,
     };
+
     this.handleInput = this.handleInput.bind(this);
     this.changeRealityClick = this.changeRealityClick.bind(this);
+
     this.searchClick = this.searchClick.bind(this);
     this.searchCharacter = this.searchCharacter.bind(this);
+
     this.nextPage = this.nextPage.bind(this);
     this.previousPage = this.previousPage.bind(this);
   }
@@ -106,18 +111,15 @@ class StrangerThings extends React.Component {
           hereIsTheUpsideDownWorld,
         )}` }
       >
-        <h2>
-          { process.env.REACT_APP_DEVELOPMENT_MODE === 'true' ?
-          'Em desenvolvimento' :
-          '' }
-        </h2>
         <div className="content strangerfy">
           <div className="change-reality">
             <button type="button" onClick={ this.changeRealityClick }>
               {' '}
               Mudar de Realidade
             </button>
+            { developmentEnvironment && <span>Em desenvolvimento</span> }
           </div>
+
           <div>
             <input
               placeholder="Nome do Personagem"
